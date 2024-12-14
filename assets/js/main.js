9cb81c737f2cc1cad98b06c5118b2d4a2a8dda79
Balula12 <gugabasigb2@gmail.com>
2024-12-14 16:42:30 -0300

    feat: pagination button e mecanismo de paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,34 @@
+const pokemonList = document.getElementById('pokemons')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 12;
+let offset = 0;
 
 
-function convertPokemonToHtml(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}" >
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-                
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>` ).join('')}
-                    </ol>
-
-                    <img src="${pokemon.photo}" 
-                         alt="${pokemon.name}">
-                </div>
-            </li>
-    `
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}" >
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+                    
+                    <div class="detail">
+                        <ol class="types">
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>` ).join('')}
+                        </ol>
+    
+                        <img src="${pokemon.photo}" 
+                             alt="${pokemon.name}">
+                    </div>
+                </li>
+        `).join('')
+        pokemonList.innerHTML = newHtml 
+    })
 }
 
+loadPokemonItens(offset, limit)
 
-const pokemonList = document.getElementById('pokemons')
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToHtml).join('')
-    pokemonList.innerHTML = newHtml 
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
